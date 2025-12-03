27faf2652a328f7168e21e14bb190c05facbdc36
Vikindor <vikindor@proton.me>
2025-12-03 17:11:12 +0300

    chore: update dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("com.codeborne:selenide:7.12.1")
-    testImplementation(platform("org.junit:junit-bom:6.0.0-M2"))
+    testImplementation("com.codeborne:selenide:7.12.2")
+    testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("io.rest-assured:rest-assured:5.5.6")
     testImplementation("io.rest-assured:json-schema-validator:5.5.6")
